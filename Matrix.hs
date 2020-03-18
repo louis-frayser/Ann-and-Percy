c9461af0575508e1be3257ee5fbd358950eb72d2
Louis Frayser <louis.frayser@gmail.com>
2020-03-18 02:08:35 -0700

    1st working version

---
+++ Matrix.hs
@@ -0,0 +1,28 @@
+module Matrix (F,hmul, mmult, transpose,  mdec, mmap, madd,
+               matsub)
+where
+import Data.List
+
+type F = [[Float]]
+
+mmult :: Num a => [[a]] -> [[a]] -> [[a]] 
+mmult a b = [ [ sum $ zipWith (*) ar bc | bc <- (transpose b) ] | ar <- a ]
+
+hmul :: Num a => [[a]] -> [[a]] -> [[a]] -- Hardamard
+hmul (m:ms) (n:ns) = (zipWith (*) m n):(hmul ms ns)
+hmul _ _ = []
+
+mmap ::  (Num a, Num b ) => (a -> b) -> [[a]] -> [[b]]
+mmap f mat@(v:vs) = (map f v):(mmap f vs)
+mmap _ _ = []
+
+mdec :: Num a => [[a]]->[[a]]
+mdec a = mmap (\x ->  -x) a
+
+mapp2 :: Num a => (a -> a -> a) -> [[a]] -> [[a]] -> [[a]] -- Hardamard
+mapp2 f (m:ms) (n:ns) = (zipWith f  m n):(mapp2 f ms ns)
+mapp2 _ _ _ = []
+
+matsub,madd :: [[Float]] -> [[Float]] -> [[Float]]
+madd = mapp2 (+)
+matsub = mapp2 (-)
---
+++ perceptron.hs
@@ -0,0 +1,34 @@
+import Matrix
+import System.Random
+
+training_inputs=[[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]] -- ::[[Float]]
+training_outputs=transpose [[0, 1, 1, 0]]::[[Float]]
+
+sigmoid x =  1 / (1 + exp (-x))
+sigderiv sx = sx * (1 - sx)
+
+ann :: F -> F -> F -> Int -> F
+ann  trn_in weights trn_out iter =
+  let outf w_in   =  mmap sigmoid (trn_in `mmult` w_in)
+      errf out = matsub trn_out out
+      adjf out err =  err `hmul` (mmap sigderiv out)
+      weightf w o adj = madd w ((transpose trn_in) `mmult` adj)
+      out_wout w0 = let out = outf w0; err = errf out; adj=adjf out err; w = weightf w0 out adj in (out, w)
+      
+      ann' :: F -> F -> Int -> F
+      ann' o w i = if i <= 0 then o else let (o',w') = out_wout w in ann' o' w'  (i - 1)
+      (out,weights') = out_wout weights
+   in ann' out weights' (iter -1)
+                   
+main = 
+  do rx <- getStdGen >>= return . randoms >>= return . take 3 :: IO [Float]
+     let synaptic_weights = transpose [ map (\r -> (2 * r) - 1) rx ]
+     
+     putStrLn "Inputs:"
+     print training_inputs
+     
+     putStrLn "Random starting synaptic weights:"
+     putStrLn $ show synaptic_weights
+     
+     let result = "\nOutput after training:\n" ++ show ( ann training_inputs synaptic_weights training_outputs 20000)
+     putStrLn  result
